0b28bd985e65c98e3654eb4e17f08856011bdf1d
SayaliNikumbh25 <sayalinikumbh7890@gmail.com>
2024-03-17 23:22:13 +0530

    initia

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+function clickCounter() {
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount)+1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+  document.getElementById("demo").innerHTML = localStorage.clickcount;
+}
